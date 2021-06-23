c5deeaeb2fa596238ba0eb3c0c92c3edf046d37e
ja <ivananl@yahoo.com>
2021-06-23 16:13:58 +0200

    Article model with images

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ApplicationRecord
+    mount_uploader :image, CoverUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   post   'login',  to: 'sessions#create', as: "login_path"
   delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
 end
