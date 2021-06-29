1bd8762e9db1926ada4f91ce69974c634d16be3f
ja <ivananl@yahoo.com>
2021-06-29 17:20:39 +0200

    Fixed problem with loading empty site

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,12 +3,12 @@ class CategoriesController < ApplicationController
 
   def index
     @articles = Article.all.newest_first
-    @top = Vote.most_popular.includes(article: [:category]).first
-    @topcover = Article.where(category_id: 1).newest_first.first
-    @topnews = Article.where(category_id: 2).newest_first.first
-    @topinterviews = Article.where(category_id: 3).newest_first.first
-    @toplive = Article.where(category_id: 4).newest_first.first
-    @topreview = Article.where(category_id: 5).newest_first.first
+    @top = Vote.most_popular.includes(article: [:category]).limit(1)
+    @topcover = Article.where(category_id: 1).newest_first.limit(1)
+    @topnews = Article.where(category_id: 2).newest_first.limit(1)
+    @topinterviews = Article.where(category_id: 3).newest_first.limit(1)
+    @toplive = Article.where(category_id: 4).newest_first.limit(1)
+    @topreview = Article.where(category_id: 5).newest_first.limit(1)
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 20 }
-  has_many :articles
+  has_many :articles, foreign_key: 'author_id', class_name: 'Article'
   has_many :votes
 end
